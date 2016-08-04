74e99e45685463b0321428b17cec3753ae55a483
Prashant Raghav <prashantraghav60@gmail.com>
2016-08-04 14:52:06 -0400

    logger removed from court complex and establishement

--- app/models/court_complex.rb
+++ app/models/court_complex.rb
@@ -2,7 +2,6 @@ class CourtComplex < Ecourt
   belongs_to :search
 
   def result
-    logger.info("\n\n\n CourtComplext - #{self.inspect} \n\n\n")
     get_result({:court_code_arr=>court_code})
   end
 end
