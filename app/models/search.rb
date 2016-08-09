db522f20bc37f0d56d64f80867492e7bdccb0052
Prashant Raghav <prashantraghav60@gmail.com>
2016-08-09 03:16:11 -0400

    fixes for stats

--- app/models/search.rb
+++ app/models/search.rb
@@ -6,13 +6,13 @@ class Search < ActiveRecord::Base
 
   default_scope {where('user_id != ?', User.first.id)}
 
-  scope :today, ->{where('DATE(created_at) = ?', Time.now)}
-  scope :yesterday, ->{where('DATE(created_at) = ?', Time.now.yesterday)}
+  scope :today, ->{where('DATE(created_at) = DATE(?)', Time.now)}
+  scope :yesterday, ->{where('DATE(created_at) = DATE(?)', Time.now.yesterday)}
   
-  scope :this_week, ->{where('Date(created_at) >=? and DATE(created_at) <= ?', Time.now.beginning_of_week, Time.now)}
-  scope :last_week, ->{where('Date(created_at) >=? and DATE(created_at) <= ?', (Time.now - 1.week).beginning_of_week, (Time.now - 1.week).end_of_week)}
+  scope :this_week, ->{where('Date(created_at) >=DATE(?) and DATE(created_at) <= DATE(?)', Time.now.beginning_of_week, Time.now)}
+  scope :last_week, ->{where('Date(created_at) >=DATE(?) and DATE(created_at) <= DATE(?)', (Time.now - 1.week).beginning_of_week, (Time.now - 1.week).end_of_week)}
 
-  scope :this_month, ->{where('Date(created_at) >=? and DATE(created_at) <= ?', Time.now.beginning_of_month, Time.now)}
-  scope :last_month, ->{where('Date(created_at) >=? and DATE(created_at) <= ?', (Time.now - 1.month).beginning_of_month , (Time.now - 1.month).end_of_month)}
+  scope :this_month, ->{where('Date(created_at) >=DATE(?) and DATE(created_at) <= DATE(?)', Time.now.beginning_of_month, Time.now)}
+  scope :last_month, ->{where('Date(created_at) >=DATE(?) and DATE(created_at) <= DATE(?)', (Time.now - 1.month).beginning_of_month , (Time.now - 1.month).end_of_month)}
 
 end
