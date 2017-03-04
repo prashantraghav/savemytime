0d53a2b0b1b20c1df65a23859056a7874d7c2761
Prashant Raghav <prashantraghav60@gmail.com>
2017-03-04 00:30:08 -0500

    unscoped kase on edit

--- app/controllers/kases_controller.rb
+++ app/controllers/kases_controller.rb
@@ -71,7 +71,7 @@ class KasesController < ApplicationController
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_kase
-    @kase = Kase.find(params[:id])
+    @kase = Kase.unscoped.find(params[:id])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
